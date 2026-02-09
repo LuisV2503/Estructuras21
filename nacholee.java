811f7d4f20d90eb00a72f92a53cd3377392d3af0
Sala K 405 <salak405@salasfr.itm.edu.co>
2026-02-09 09:10:52 -0500

    Se elimina el Hola mundo y se crea logicas para sumar dos numeros enteros ingresados por consola

--- nacholee.java
+++ nacholee.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class nacholee {
     public static void main(String[] args) {
         
-        System.out.println("Hola mundo");
+        int n1 =0, n2 = 0, n3=0;
+        System.out.println("Vamos a sumar dos numeros");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ingrese el numero 1");
+        n1 = sc.nextInt();
+        System.out.println("Inrese el numero 2");
+        n2 = sc.nextInt();
+        System.out.println("La suma de los dos numeros es: "+ (n1+n2));
+
     }
 }
